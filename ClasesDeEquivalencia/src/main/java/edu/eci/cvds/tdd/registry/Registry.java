2c64ad796bf36d885df4d709ced12418fa476b27
Nicole Montaña <nicole.montana@mail.escuelaing.edu.co>
2022-02-10 00:12:06 -0500

    Modificación registro

--- ClasesDeEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesDeEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,6 +1,9 @@
 package edu.eci.cvds.tdd.registry;
 
 public class Registry {
+
+    HashMap<Integer, String> listRegistry = new HashMap<Integer, String>();
+
     public RegisterResult registerVoter(Person p) {
         RegisterResult result = null;
         if (!p.isAlive()){
@@ -11,6 +14,10 @@ public class Registry {
             result = RegisterResult.INVALID_AGE;
         }else if (p.getAge() >= 18 && p.isAlive()){
             result = RegisterResult.VALID;
+        }else if (listRegistry.containsKey(p.getId())){
+            result = RegisterResult.DUPLICATED;
+        }else{
+            listRegistry.put(p.getId(), p.getName());
         }
 
         return result;
