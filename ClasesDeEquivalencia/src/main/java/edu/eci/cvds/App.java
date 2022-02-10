ac24a8f2e33da0cd1988f5ff32befbff3b25a9d9
Nicole Montaña <nicole.montana@mail.escuelaing.edu.co>
2022-02-09 23:19:30 -0500

    Cambios

--- ClasesDeEquivalencia/src/main/java/edu/eci/cvds/App.java
+++ ClasesDeEquivalencia/src/main/java/edu/eci/cvds/App.java
@@ -6,13 +6,8 @@ package edu.eci.cvds;
  */
 public class App 
 {
-    public static void main(String[] args )
+    public static void main( String[] args )
     {
-        if(args.length == 0){
-            System.out.println("Hello World!");
-        }
-        else{
-            System.out.println("Hello " + String.join(" ",args) + "!");
-        }
+        System.out.println( "Hello World!" );
     }
 }
