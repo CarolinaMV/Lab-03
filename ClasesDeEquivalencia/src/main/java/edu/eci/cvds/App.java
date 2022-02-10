5ecfb925a388ecf2321151de0fa4e5b8e75d7d4b
Nicole Montaña <nicole.montana@mail.escuelaing.edu.co>
2022-02-09 22:16:26 -0500

    Agregar esqueleto

---
+++ ClasesDeEquivalencia/src/main/java/edu/eci/cvds/App.java
@@ -0,0 +1,18 @@
+package edu.eci.cvds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+        if(args.length == 0){
+            System.out.println("Hello World!");
+        }
+        else{
+            System.out.println("Hello " + String.join(" ",args) + "!");
+        }
+    }
+}
